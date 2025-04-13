7038d2c7d4b352105bfbceb5e9cdb6b075f263ca
Ritabrata Ghosh <76sonali40@gmail.com>
2025-04-13 12:06:58 +0530

    date modified

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -5,7 +5,12 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="h-screen flex justify-center items-center flex-col">
-      <Image src="/404.jpg" width={500} height={500} alt="404" />
+      <Image
+        src="https://assignment-renit.onrender.com/404.jpg"
+        width={500}
+        height={500}
+        alt="404"
+      />
       <Link href="/">
         <Button className="cursor-pointer">Return Home</Button>
       </Link>

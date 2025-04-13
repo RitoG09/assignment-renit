78146c650a29621937f9d5254480197e31f74c87
Ritabrata Ghosh <76sonali40@gmail.com>
2025-04-13 12:19:00 +0530

    404 photo update

--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,16 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="h-screen flex justify-center items-center flex-col">
-      <Image
-        src="/404.jpg"
-        width={500}
-        height={500}
-        alt="404"
-      />
+      <img src="/404.jpg" width={500} height={500} alt="404" />
       <Link href="/">
         <Button className="cursor-pointer">Return Home</Button>
       </Link>
